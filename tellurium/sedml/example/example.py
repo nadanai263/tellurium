e94d17dbf963e087398c09106e661b28bd0b09d1
Matthias Koenig <konigmatt@googlemail.com>
2017-10-07 10:14:42 +0200

    working on refactoring tesedml

---
+++ tellurium/sedml/example/example.py
@@ -0,0 +1,19 @@
+"""
+Testing of tesedml
+"""
+
+from __future__ import print_function, absolute_import
+import os
+import tellurium as te
+from tellurium.tests.testdata import SEDML_TEST_DIR, OMEX_TEST_DIR
+
+
+
+if __name__ == "__main__":
+    outputDir = "./__tmp__"
+    if not os.path.exists(outputDir):
+        os.makedirs(outputDir)
+
+
+    test_omex = os.path.join(OMEX_TEST_DIR, 'specification', 'L1V3', 'L1V3_parameter-scan-2d.omex')
+    dgs = te.executeCombineArchive(test_omex, printPython=True, outputDir=outputDir, saveOutputs=True)
--- tellurium/sedml/tesedml.py
+++ tellurium/sedml/tesedml.py
@@ -4,17 +4,21 @@ Tellurium SED-ML support.
 
 This module implements SED-ML support for tellurium.
 
-----------------
+------------------------------------
 Overview SED-ML
-----------------
+------------------------------------
 SED-ML is build of main classes
-    the Model Class,
-    the Simulation Class,
-    the Task Class,
-    the DataGenerator Class,
-    and the Output Class.
+    DataDescription
+    Model,
+    Simulation,
+    Task,
+    DataGenerator,
+    Output.
 
-The Model Class
+DataDescription
+    External data can be specificed in SED-ML via the DataDescription.
+
+Model
     The Model class is used to reference the models used in the simulation experiment.
     SED-ML itself is independent of the model encoding underlying the models. The only
     requirement is that the model needs to be referenced by using an unambiguous identifier
@@ -26,10 +30,7 @@ The Model Class
     of the model representation that is addressable by XPath expressions, e.g. substituting
     a piece of XML by an updated one.
 
-TODO: DATA CLASS
-
-
-The Simulation Class
+Simulation
     The Simulation class defines the simulation settings and the steps taken during simulation.
     These include the particular type of simulation and the algorithm used for the execution of
     the simulation; preferably an unambiguous reference to such an algorithm should be given,
@@ -38,12 +39,12 @@ The Simulation Class
     in the Simulation class includes the step size, simulation duration, and other
     simulation-type dependent information.
 
-The Task Class
+Task
     SED-ML makes use of the notion of a Task class to combine a defined model (from the Model class)
     and a defined simulation setting (from the Simulation class). A task always holds one reference each.
     To refer to a specific model and to a specific simulation, the corresponding IDs are used.
 
-The DataGenerator Class
+DataGenerator
     The raw simulation result sometimes does not correspond to the desired output of the simulation,
     e.g. one might want to normalise a plot before output, or apply post-processing like mean-value calculation.
     The DataGenerator class allows for the encoding of such post-processings which need to be applied to the
@@ -51,14 +52,13 @@ The DataGenerator Class
     of any defined model (from instances of the Model class) may be referenced, and new entities might
     be specified using MathML definitions.
 
-The Output Class
+Output
     The Output class defines the output of the simulation, in the sense that it specifies what shall be
     plotted in the output. To do so, an output type is defined, e.g. 2D-plot, 3D-plot or data table,
     and the according axes or columns are all assigned to one of the formerly specified instances
     of the DataGenerator class.
 
-For information about SED-ML please refer to http://www.sed-ml.org/
-and the SED-ML specification.
+For information about SED-ML please refer to http://www.sed-ml.org/.
 
 ------------------------------------
 SED-ML in tellurium: Implementation
@@ -66,31 +66,24 @@ SED-ML in tellurium: Implementation
 SED-ML support in tellurium is based on Combine Archives.
 The SED-ML files in the Archive can be executed and stored with results.
 
-----------------------------------------
-SED-ML in tellurium: Supported Features
-----------------------------------------
-Tellurium supports SED-ML L1V3 with SBML as model format.
+Tellurium implements full support for SED-ML L1V3 with SBML as model format.
+For CellML models only basic support is implemented. CellML models are converted
+to SBML models. This results in different XPath expressions, in the SBML model
+so that XPath targets cannot be easily resolved in the CellMl-SBML.
 
-SBML models are fully supported, whereas for CellML models only basic support
-is implemented (when additional support is requested it be implemented).
-CellML models are transformed to SBML models which results in different XPath expressions,
-so that targets, selections cannot be easily resolved in the CellMl-SBML.
-
-Supported input for SED-ML are either SED-ML files ('.sedml' extension),
-SED-ML XML strings or combine archives ('.sedx'|'.omex' extension).
 Executable python code is generated from the SED-ML which allows the
 execution of the defined simulation experiment.
 
-    In the current implementation all SED-ML constructs with exception of
-    XML transformation changes of the model
-        - Change.RemoveXML
-        - Change.AddXML
-        - Change.ChangeXML
-    are supported.
+In the current implementation all SED-ML constructs with exception of
+XML transformation changes of the model
+    - Change.RemoveXML
+    - Change.AddXML
+    - Change.ChangeXML
+and complex XPath expressions are supported.
 
--------
+------------------------------------
 Notice
--------
+------------------------------------
 The main maintainer for SED-ML support is Matthias König.
 Please let changes to this file be reviewed and make sure that all SED-ML related tests are working.
 """
@@ -119,14 +112,6 @@ from tellurium.utils import omex
 from .mathml import evaluableMathML
 import tellurium as te
 
-try:
-    # required imports in generated python code
-    import pandas
-    import matplotlib.pyplot as plt
-    import mpl_toolkits.mplot3d
-except ImportError:
-    warnings.warn("Dependencies for SEDML code execution not fulfilled.")
-    print(traceback.format_exc())
 
 ######################################################################################################################
 # KISAO MAPPINGS
@@ -336,7 +321,6 @@ def executeCombineArchive(omexPath,
                 warnings.warn(
                     "No SED-ML files in COMBINE archive based on manifest '{}'; Guessed SED-ML {}".format(omexPath, sedml_locations))
 
-
             # run all sedml files
             results = {}
             sedml_paths = [os.path.join(extractDir, loc) for loc in sedml_locations]
@@ -363,8 +347,8 @@ def executeCombineArchive(omexPath,
         else:
             raise IOError("File is not an OMEX Combine Archive in zip format: {}".format(omexPath))
 
-######################################################################################################################
 
+######################################################################################################################
 class SEDMLCodeFactory(object):
     """ Code Factory generating executable code."""
 
@@ -404,7 +388,6 @@ class SEDMLCodeFactory(object):
         if self.outputDir:
             if not os.path.exists(outputDir):
                 raise IOError("outputDir does not exist: {}".format(outputDir))
-
 
         info = SEDMLTools.readSEDMLDocument(inputStr, workingDir)
         self.doc = info['doc']
@@ -621,7 +604,6 @@ class SEDMLCodeFactory(object):
 
         return lines
 
-
     def dataDescriptionToPython(self, dataDescription):
         """ Python code for DataDescription.
 
@@ -660,25 +642,23 @@ class SEDMLCodeFactory(object):
     def taskToPython(doc, task):
         """ Create python for arbitrary task (repeated or simple).
 
-        :param doc:
-        :type doc:
+        Tasks contain other subtasks, which can contain subtasks. This results in a tree of task dependencies where the
+        simple tasks are the node leaves. These tree has to be resolved to generate code for
+        more complex task dependencies.
+
+        :param doc: SEDDocument
         :param task:
-        :type task:
         :return:
-        :rtype:
         """
-        # If no DataGenerator references the task, no execution is necessary
+        # DataGenerators for the given task
         dgs = SEDMLCodeFactory.getDataGeneratorsForTask(doc, task)
         if len(dgs) == 0:
+            # If no DataGenerator references the task, no execution is necessary
             return "# not part of any DataGenerator: {}".format(task.getId())
-
-        # tasks contain other subtasks, which can contain subtasks. This
-        # results in a tree of task dependencies where the
-        # simple tasks are the node leaves. These tree has to be resolved to
-        # generate code for more complex task dependencies.
 
         # resolve task tree (order & dependency of tasks) & generate code
         taskTree = SEDMLCodeFactory.createTaskTree(doc, rootTask=task)
+        print(taskTree)
         return SEDMLCodeFactory.taskTreeToPython(doc, tree=taskTree)
 
     class TaskNode(object):
@@ -1739,7 +1719,6 @@ class SEDMLTools(object):
 
     INPUT_TYPE_STR = 'SEDML_STRING'
     INPUT_TYPE_FILE_SEDML = 'SEDML_FILE'
-    INPUT_TYPE_FILE_COMBINE = 'COMBINE_FILE'  # includes .sedx archives
 
     @classmethod
     def checkSEDMLDocument(cls, doc):
@@ -1751,12 +1730,11 @@ class SEDMLTools(object):
         errorlog = doc.getErrorLog()
         msg = errorlog.toString()
         if doc.getErrorLog().getNumFailsWithSeverity(libsedml.LIBSEDML_SEV_ERROR) > 0:
-            # FIXME: workaround for https://github.com/fbergmann/libSEDML/issues/47
-            warnings.warn(msg)
-            # raise IOError(msg)
+            # warnings.warn(msg)
+            raise IOError(msg)
         if errorlog.getNumFailsWithSeverity(libsedml.LIBSEDML_SEV_FATAL) > 0:
-            # raise IOError(msg)
-            warnings.warn(msg)
+            # warnings.warn(msg)
+            raise IOError(msg)
         if errorlog.getNumFailsWithSeverity(libsedml.LIBSEDML_SEV_WARNING) > 0:
             warnings.warn(msg)
         if errorlog.getNumFailsWithSeverity(libsedml.LIBSEDML_SEV_SCHEMA_ERROR) > 0:
@@ -1764,78 +1742,50 @@ class SEDMLTools(object):
         if errorlog.getNumFailsWithSeverity(libsedml.LIBSEDML_SEV_GENERAL_WARNING) > 0:
             warnings.warn(msg)
 
+
     @classmethod
-    def readSEDMLDocument(cls, inputStr, workingDir):
-        """ Parses SedMLDocument from given input.
+    def readSEDMLDocument(cls, inputStr, workingDir=None):
+        """ Parses SedMLDocument from given input string or path.
+
+        :param inputStr: SED-ML to read, either SED-ML string or path to SED-ML file
+        :param workingDir: working directory relative to which the SED-ML resources are resolved.
 
         :return: dictionary of SedDocument, inputType and working directory.
         :rtype: {doc, inputType, workingDir}
         """
-
         # SEDML-String
         if not os.path.exists(inputStr):
+            inputType = cls.INPUT_TYPE_STR
+            if workingDir is None:
+                workingDir = os.getcwd()
+
             try:
+                # check if xml can be parsed
                 from xml.etree import ElementTree
                 x = ElementTree.fromstring(inputStr)
                 # is parsable xml string
                 doc = libsedml.readSedMLFromString(inputStr)
-                inputType = cls.INPUT_TYPE_STR
-                if workingDir is None:
-                    workingDir = os.getcwd()
-
             except ElementTree.ParseError:
-                if not os.path.exists(inputStr):
-                    raise IOError("SED-ML String is not valid XML:", inputStr)
+                raise IOError("SED-ML String is not valid XML:", inputStr)
 
         # SEDML-File
         else:
+            inputType = cls.INPUT_TYPE_FILE_SEDML
+            if not os.path.isfile(inputStr):
+                raise IOError("SED-ML path is not a file:", inputStr)
+
             filename, extension = os.path.splitext(os.path.basename(inputStr))
+            if extension not in [".sedml", '.xml']:
+                warnings.warn("SEDML file should have [.sedml|.xml] extension:", inputStr)
 
-            # Archive
-            if zipfile.is_zipfile(inputStr):
-                omexPath = inputStr
-                inputType = cls.INPUT_TYPE_FILE_COMBINE
+            doc = libsedml.readSedMLFromFile(inputStr)
 
-                # in case of sedx and combine a working directory is created
-                # in which the files are extracted
-                if workingDir is None:
-                    extractDir = os.path.join(os.path.dirname(os.path.realpath(omexPath)), '_te_{}'.format(filename))
-                else:
-                    extractDir = workingDir
+            # working directory is where the sedml file is
+            if workingDir is None:
+                workingDir = os.path.dirname(os.path.realpath(inputStr))
 
-
-                # TODO: refactor this
-                # extract the archive to working directory
-                CombineArchive.extractArchive(omexPath, extractDir)
-                # get SEDML files from archive
-                sedmlFiles = CombineArchive.filePathsFromExtractedArchive(extractDir, filetype='sed-ml')
-
-                if len(sedmlFiles) == 0:
-                    raise IOError("No SEDML files found in archive.")
-
-                # FIXME: there could be multiple SEDML files in archive (currently only first used)
-                # analogue to executeOMEX
-                if len(sedmlFiles) > 1:
-                    warnings.warn("More than one sedml file in archive, only processing first one.")
-
-                sedmlFile = sedmlFiles[0]
-                doc = libsedml.readSedMLFromFile(sedmlFile)
-                # we have to work relative to the SED-ML file
-                workingDir = os.path.dirname(sedmlFile)
-
-                cls.checkSEDMLDocument(doc)
-
-
-            # SEDML single file
-            elif os.path.isfile(inputStr):
-                if extension not in [".sedml", '.xml']:
-                    raise IOError("SEDML file should have [.sedml|.xml] extension:", inputStr)
-                inputType = cls.INPUT_TYPE_FILE_SEDML
-                doc = libsedml.readSedMLFromFile(inputStr)
-                cls.checkSEDMLDocument(doc)
-                # working directory is where the sedml file is
-                if workingDir is None:
-                    workingDir = os.path.dirname(os.path.realpath(inputStr))
+        # check the document
+        cls.checkSEDMLDocument(doc)
 
         return {'doc': doc,
                 'inputType': inputType,
@@ -1873,9 +1823,16 @@ class SEDMLTools(object):
                 changes.append(c)
             model_changes[mid] = changes
 
-        # recursive search for original model and store the
-        # changes which have to be applied in the list of changes
+        #
         def findSource(mid, changes):
+            """ Recursive search for original model and store the
+            changes which have to be applied in the list of changes.
+
+            :param mid:
+            :param changes:
+            :return:
+            """
+
             # mid is node above
             if mid in model_sources and not model_sources[mid] == mid:
                 # add changes for node
@@ -1895,38 +1852,3 @@ class SEDMLTools(object):
             all_changes[mid] = changes[::-1]
 
         return model_sources, all_changes
-
-
-##################################################################################################
-if __name__ == "__main__":
-    import os
-    from tellurium.tests.testdata import SEDML_TEST_DIR, OMEX_TEST_DIR
-    import matplotlib
-
-    def testInput(sedmlInput):
-        """ Test function run on inputStr. """
-        print('\n', '*'*100)
-        print(sedmlInput)
-        print('*'*100)
-        factory = SEDMLCodeFactory(sedmlInput)
-
-        # create python file
-        python_str = factory.toPython()
-        realPath = os.path.realpath(sedmlInput)
-        with open(sedmlInput + '.py', 'w') as f:
-            f.write(python_str)
-
-        # execute python
-        factory.executePython()
-
-    # testInput(os.path.join(sedmlDir, "sedMLBIOM21.sedml"))
-
-    # Check sed-ml files
-    for fname in sorted(os.listdir(SEDML_TEST_DIR)):
-        if fname.endswith(".sedml"):
-            testInput(os.path.join(SEDML_TEST_DIR, fname))
-
-    # Check sedx archives
-    for fname in sorted(os.listdir(OMEX_TEST_DIR)):
-        if fname.endswith(".sedx"):
-            testInput(os.path.join(OMEX_TEST_DIR, fname))
